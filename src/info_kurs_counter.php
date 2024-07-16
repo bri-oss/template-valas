ddf942e125cbded3ee3f1db60b47676925f33703
AlfaRiza <malfariza45@gmail.com>
2024-07-16 10:22:34 +0700

    update: change get token snap to bri api

--- src/info_kurs_counter.php
+++ src/info_kurs_counter.php
@@ -12,7 +12,6 @@ require __DIR__ . '/../../briapi-sdk/autoload.php';
 
 $clientId = $_ENV['CONSUMER_KEY']; // customer key
 $clientSecret = $_ENV['CONSUMER_SECRET']; // customer secret
-$pKeyId = $_ENV['PRIVATE_KEY']; // private key
 
 // url path values
 $baseUrl = 'https://sandbox.partner.api.bri.co.id'; //base url
@@ -29,8 +28,6 @@ $accessToken = $getAccessToken->getBRIAPI(
   $clientSecret,
   $baseUrl
 );
-echo $accessToken;
-return;
 
 $valas = new Valas();
 
