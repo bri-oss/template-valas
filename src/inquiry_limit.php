70b721161881d549f9c98db5c9e2eb77caab0697
AlfaRiza <malfariza45@gmail.com>
2024-09-13 17:28:24 +0700

    update: add variable inquiry limit

--- src/inquiry_limit.php
+++ src/inquiry_limit.php
@@ -29,8 +29,8 @@ $date = new DateTime("now", new DateTimeZone("UTC"));
 
 $timestamp = $date->format('Y-m-d\TH:i:s') . '.' . substr($date->format('u'), 0, 3) . 'Z';
 
-$debitAccount = '';
-$partnerCode = '';
+$debitAccount = '020602000008513';
+$partnerCode = 'rxEG1EMYHQZMgb3';
 
 $response = $valas->inquiryLimit(
   $clientSecret,
